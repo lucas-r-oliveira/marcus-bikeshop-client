9d509493c95cefb9ee30a3346727229eeb287712
lucas-r-oliveira <lucasoliveira7c@gmail.com>
2025-03-14 19:33:58 +0000

    ProductCard component and product grid

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router';
+import './ProductCard.css';
+
+interface ProductCardProps {
+	productId: string;
+	name: string; // can also be title
+	price: number;
+	currency: string;
+	imageUrl: string;
+	description: string;
+	inStock: boolean;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function handleAddToCart() {}
+
+export default function ProductCard(props: ProductCardProps) {
+	return(
+		<Link to={`/products/${props.productId}`}>
+			<div className='card-container'>
+				<img alt='' src={props.imageUrl}></img>
+				<div className='text-info-container'>
+					<h4> {props.name} </h4>
+					<p> {props.price} {props.currency} </p>
+					<p> {props.description} </p>
+				</div>
+			</div>
+		</Link>
+	)
+}
--- src/pages/BikeGridPage.tsx
+++ src/pages/BikeGridPage.tsx
@@ -1,8 +1,55 @@
+import './BikeGridPage.css';
+import ProductCard from '../components/ProductCard';
+
 export default function BikeGridPage() {
 
 	return (
-		<div>
-			Bike Grid Page	
+		<div className="grid-container">
+		  <ProductCard 
+			productId='p1'
+			name='Mountain Bike'
+			description='The greatest for mountain biking'
+			price={42}
+			currency='€'
+			imageUrl='/path/to/p1-bike'
+			inStock={true}
+		  />
+		  <ProductCard 
+			productId='p2'
+			name='Road Bike'
+			description='Lightweight and fast for road cycling'
+			price={899}
+			currency='€'
+			imageUrl='/path/to/p2-bike'
+			inStock={true}
+		  />
+		  <ProductCard 
+			productId='p3'
+			name='Gravel Bike'
+			description='Versatile for both road and off-road adventures'
+			price={749}
+			currency='€'
+			imageUrl='/path/to/p3-bike'
+			inStock={false}
+		  />
+		  <ProductCard 
+			productId='p4'
+			name='Electric Bike'
+			description='Pedal-assist for effortless commuting'
+			price={1299}
+			currency='€'
+			imageUrl='/path/to/p4-bike'
+			inStock={true}
+		  />
+		  <ProductCard 
+			productId='p5'
+			name='BMX Bike'
+			description='Perfect for tricks and stunts'
+			price={349}
+			currency='€'
+			imageUrl='/path/to/p5-bike'
+			inStock={true}
+		  />
 		</div>
-	)
+	  )
   }
