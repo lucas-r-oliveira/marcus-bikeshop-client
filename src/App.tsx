8f31fa800a3b34392ddf0e220adb990f1682850c
lucas-r-oliveira <lucasoliveira7c@gmail.com>
2025-03-25 22:25:52 +0000

    home page get started button redirects to bicycles page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { useNavigate } from 'react-router'
 import './App.css'
 
 function App() {
+	const navigate = useNavigate();
 
   return (
     <>
 		<div>
 			<h1>WELCOME!</h1>
 			<h3>To Marcus' Bikeshop. Buy bikes and more!</h3>
-			<button>Get Started</button>
+			<button onClick={() => navigate("/products/bicycles")}>Get Started</button>
 		</div>
     </>
   )
